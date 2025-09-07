0f5279ff5b011c0110d97301ea37da688a0a42c4
ptppthanhphat-debug <ptp.pthanhphat@gmail.com>
2025-09-07 11:06:45 +0700

    first commit

---
+++ tutorial/dataType.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main () {
+  // Creating variables
+  // Standard example
+  int myNum = 5;               // Integer (whole number)
+  float myFloatNum = 5.99;     // Floating point number
+  double myDoubleNum = 9.98;   // Floating point number
+  char myLetter = 'D';         // Character
+  bool myBoolean = true;       // Boolean
+  string myString = "Hello";   // String
+   
+  // Print variable values
+  cout << "=================Standard Example==================\n";
+  cout << "int: " << myNum << "\n";
+  cout << "float: " << myFloatNum << "\n";
+  cout << "double: " << myDoubleNum << "\n";
+  cout << "char: " << myLetter << "\n";
+  cout << "bool: " << myBoolean << "\n";
+  cout << "string: " << myString << "\n";
+  cout << "===================================================\n";
+ 
+
+  // Example 1: Vi du nay khai thac van de gi
+  return 0;
+}
