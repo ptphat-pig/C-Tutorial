b95b664ab05bb0c947a58ffc37faa22c4eae12aa
ptppthanhphat-debug <ptp.pthanhphat@gmail.com>
2025-09-07 11:17:14 +0700

    format code in dataType.cpp file

--- tutorial/dataType.cpp
+++ tutorial/dataType.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
- 
-int main () {
+
+int main()
+{
   // Creating variables
   // Standard example
-  int myNum = 5;               // Integer (whole number)
-  float myFloatNum = 5.99;     // Floating point number
-  double myDoubleNum = 9.98;   // Floating point number
-  char myLetter = 'D';         // Character
-  bool myBoolean = true;       // Boolean
-  string myString = "Hello";   // String
-   
+  int myNum = 5;             // Integer (whole number)
+  float myFloatNum = 5.99;   // Floating point number
+  double myDoubleNum = 9.98; // Floating point number
+  char myLetter = 'D';       // Character
+  bool myBoolean = true;     // Boolean
+  string myString = "Hello"; // String
+
   // Print variable values
   cout << "=================Standard Example==================\n";
   cout << "int: " << myNum << "\n";
