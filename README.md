aa73d55af533aa14218c2fe8f22f9c06578e1f00
ptppthanhphat-debug <ptp.pthanhphat@gmail.com>
2025-09-09 15:39:28 +0700

    Add README.md

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## PURPOSE
 +  To learn introduction basic C/C++
 +  To practice code example C/C++
-+  To work exercise end of the year
++  To work exercise C/C++ end of the year 
 +  To learn advanced knowledge C/C++
 
  
--- tutorial/variables.cpp
+++ tutorial/variables.cpp
@@ -1,3 +1,75 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    cout << "=================Standard Example==================\n";
+    // creating variables
+    // standard example
+    char myName = 'P'; // my name is Pham Thanh Phat
+    char mySex = 'm'; // my sex is male
+    int myAge = 18; // my age is 18
+    int myYear = 2007; // I was born is 2007
+    string myAddress = "Ho Chi Minh City"; // my address is Ho Chi Minh City
+    string myCountryside = "Viet Nam"; //my countryside is Viet Nam
+    double myHeight = 1.7; // my height is 1.7m
+    int myWeight = 75; // my weight is 75kg
+    bool mySchool = true; // I am a student
+    char mygrade = 'U'; // my grade is University
+    double myGPA = 8.53; // my GPA is 8.53
+    string myUniversity = "HUFLIT"; // my university is HUFLIT
+    string myMajor = "Software Engineer"; // my major is Software Engineer
+    string myGraduateYear = "2025"; // my graduate year is 2025
+    string mySkill = "Creative, hardworking, communicative"; // my skills are Creative, hardworking, communicate
+    string myCalculationSkill = "can perform simple calculations on c/c++"; // my calculation skill is can perform simple calculations on C/C++
+    // print variable values
+    cout << "=============================================================\n";
+    // Example 1:   This example is about basic calculus.
+    string myBasicMathSkill = "can perform basic math operations such as addition, subtraction multiplication, and division"; // my basic math skill is can perform basic math operations such as addtion, subtraction multiplication, and division 
+    int sum1 = 500 + 500; // 500 + 500 = 1000
+    int sum2 = sum1 + 500; // 1000 + 500 = 1500
+    int sum3 = sum1 + sum2; // 1000 + 1500 = 2500
+    cout << "=============================================================\n";
+    // Example 2: This example is about variable declaration.
+    string example = "This is an example of variable declaration in C++"; // this is an example of variable declaration in C++
+    int i = 0;
+    for (i = 0; i < 100; i++) {
+        // Loop body can be empty or you can add code here if needed
+    } // declare an integer variable i and initialize it to 0, then increment it by 1 until it reaches 100
+    cout << "=============================================================\n";
+    // Example 3: This example is about if....else.
+    // Print variable values
+    cout << "char: " << myName << "\n";
+    cout << "char: " << mySex << "\n";
+    cout << "int: " << myAge << "\n";
+    cout << "int: " << myYear << "\n";
+    cout << "string: " << myAddress << "\n";
+    cout << "string: " << myCountryside << "\n";
+    cout << "double: " << myHeight << "\n";
+    cout << "int: " << myWeight << "\n";
+    cout << "bool: " << mySchool << "\n";
+    cout << "char: " << mygrade << "\n";
+    cout << "double: " << myGPA << "\n";
+    cout << "string: " << myUniversity << "\n";
+    cout << "string: " << myMajor << "\n";
+    cout << "string: " << myGraduateYear << "\n";
+    cout << "string: " << mySkill << "\n";
+    cout << "string: " << myCalculationSkill << "\n";
+    cout << "string: " << myBasicMathSkill << "\n";
+    cout << "int: " << sum1 << "\n";
+    cout << "int: " << sum2 << "\n";
+    cout << "int: " << sum3 << "\n";
+    cout << "string: " << example << "\n";
+    cout << "int: " << i << "\n";
+    cout << "if...else statement example:\n";
+    cout << "=================End of Standard Example=====================\n";
+    cout << "=============================================================\n";
+    cout << "=============================================================\n";
+    return 0;
+}
+
+    
+
+    
+
+    
