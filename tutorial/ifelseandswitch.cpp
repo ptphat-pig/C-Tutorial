1cdbf8546ea621e24b24c87fc28661487101c2c6
ptppthanhphat-debug <ptp.pthanhphat@gmail.com>
2025-09-14 14:52:40 +0700

    Update ifelseandswitch.cpp

--- tutorial/ifelseandswitch.cpp
+++ tutorial/ifelseandswitch.cpp
@@ -9,13 +9,17 @@ int main() {
     string example1 = "This example is about If-Else statements"; // this example is about If-Else statements
     int i = 10; // i = 10
     int h = 20; // h = 20
+    cout << " i = " << i << "\n"; //print i value
+    cout << " h = " << h << "\n"; //print h value
     if (i < h) {
-        cout << "i is less than h\n"; // if i < h. print "i is less than h"
-            cout << "i is greater than h\n"; // if i > h. print "i is greater than h"
+        cout << "i < h" << "  ;  " << "i is less than h" "\n"; // if i < h. print "i is less than h"
+            cout << "i is greater than h" "\n"; // if i > h. print "i is greater than h"
         } 
     if (i >= h) {
+        cout << "i >= h" "\n";
         cout << "false" "\n"; // if i >= h. print "false"
-    }else {
+    }   
+    else {
         cout << "true" "\n"; // else print "true
     }
     
