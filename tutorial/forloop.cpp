92733864c3b140138eb5d34edbe0e68a1e971ff2
ptppthanhphat-debug <ptp.pthanhphat@gmail.com>
2025-09-14 14:14:54 +0700

    test git commit

--- tutorial/forloop.cpp
+++
@@ -1,3 +0,0 @@
-// This file is part of the tutorial project
-// For loop example
-#include <iostream>
--- tutorial/ifelseandswitch.cpp
+++ tutorial/ifelseandswitch.cpp
@@ -1,1 +1,20 @@
 // This file is intended to demonstrate the use of if-else statements and switch-case statements in C++. It typically contains examples showing how conditional logic can be implemented to control program flow based on different values or user input.
+#include <iostream>
+using namespace std;
+using std::cout;
+#include <string>
+
+int main() {
+    // Example 1: This example is about If-Else statements
+    string example1 = "This example is about If-Else statements"; // this example is about If-Else statements
+    int i = 10; // i = 10
+    int h = 20; // h = 20
+    if (i < h) {
+        cout << "i is less than h\n"; // if i < h. print "i is less than h"
+        cout << "false]\n"; if (i>h) {
+            cout << "i is greater than h\n"; // if i > h. print "i is greater than h"
+        } else {
+            cout << "i is equal to h\n"; // if i == h. print "i is equal to h"
+        }
+
+    }
