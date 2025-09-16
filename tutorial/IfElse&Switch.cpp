1056b35904097e5e5a96bbfd95bf5189a11a3d16
ptppthanhphat-debug <ptp.pthanhphat@gmail.com>
2025-09-16 10:17:19 +0700

    Complete the example

--- tutorial/IfElse&Switch.cpp
+++ tutorial/IfElse&Switch.cpp
@@ -24,8 +24,50 @@ int main() {
     }
     
     cout << "==================Example 2===================\n";
-    // Example 2: This example is about Swtitch statements
-    string example2 = "This example is about Wsitch statements"; // this example is about Switch statements
+    // Example 2: This example is else
+    string example2 = "This example is else"; // this example is else
+    int time = 20;
+    if (time < 18) {
+    cout << "Good day.";
+    } else {
+  cout << "Good evening.";
+}
+    // Outputs "Good evening."
+    cout << "===================Example 3===================\n";
+    // Example 3: This example is about else if
+    string example3 = "This example is else if" "\n"; // this example is about else if
+    int hour = 22;
+    if (hour < 10) {
+    cout << "Good morning." "\n";
+    } 
+    else if (hour < 20) {
+    cout << "Good day." "\n";
+    } else {
+
+    cout << "Good evening." "\n";
+    }
+    cout << "=================Example 4==================\n";
+    // Example 4: This example is Short Hand If...Else
+    string example4 = "This example is Short Hand If...Else"; // this example is Short Hand If...Else
+    int a = 22;
+    string result = (a < 18)? "Good day." : "Good evening.";
+    cout  << result;
+    cout << "=================Example 5===================\n";
+    // Example 5: This example is Real-life Examples
+    string example5 = "This example is Real-life Examples"; // This example is Real-life Examples
+    int myNum = 10; // Is this a positive or negative number?
+
+    if (myNum > 0) {
+        cout << "The value is a positive number.\n";
+    } 
+    else if (myNum < 0) {
+        cout << "The Value is a negative number.\n";
+    } else {
+        cout << "The value is 0.\n";
+    }
+    cout << "==================Exampkle 6==================\n";
+    // Example 6 : This example is about Swtitch statements
+    string example = "This example is about Wsitch statements"; // this example is about Switch statements
     string myTimeTable = "I go to school on :"; // my time table is I go to school on :
     int day = 2; // 2 days
     cout << myTimeTable << "\n"; // print my time table
@@ -43,4 +85,5 @@ int main() {
             cout << "Saturday" "\n"; // print "Saturday"
        
              break;}
+    cout << "=================finish example==================\n";
 }
