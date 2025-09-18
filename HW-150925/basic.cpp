4ecbd5d120247fa1d8b30e4c5d3ebad110b6f05a
ptppthanhphat-debug <ptp.pthanhphat@gmail.com>
2025-09-18 19:46:08 +0700

    Add example 2

--- HW-150925/basic.cpp
+++ HW-150925/basic.cpp
@@ -7,30 +7,31 @@ From: 1     To: 20
 using namespace std;
 
 int main () {
+    cout << "===================Example 1=====================\n";
+    // Example 1: This example is Sum of two numbers
+    string example1 = "This example is about Sum of two numbers"; // This example is about Sum of two numbers
     int a = 29, b = 30;
     int sum = a + b;
     
     cout << "Tong cua hai so:\n"; // print the sum of two numbers
     cout << "---------------------------------\n";
-    cout << "Tong cua" << a << "va" << b << "la :" << sum << "z\n";
+    cout << "Tong cua" << a << "va" << b << "la :" << sum << "\n";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    cout << "===================Example 2=====================\n";
+    // Example 2: This example is Size of Fundamental Data Types
+    string example2 = "This example is about Size of Fundamental Data Types"; // This example is about Size of Fundamental Data Types
+    cout << "Find Size of fundamental data types" "\n"; // Find Size of fundamental data types
+    cout << "--------------------------------------\n";
+     cout << "The sizeof(char) is : " << sizeof(char) << " bytes" << endl;
+    cout << "The sizeof(short) is : " << sizeof(short) << " bytes" << endl;
+    cout << "The sizeof(int) is : " << sizeof(int) << " bytes" << endl;
+    cout << "The sizeof(long) is : " << sizeof(long) << " bytes" << endl;
+    cout << "The sizeof(long long) is : " << sizeof(long long) << " bytes" << endl;
+    cout << "The sizeof(float) is : " << sizeof(float) << " bytes" << endl;
+    cout << "The sizeof(double) is : " << sizeof(double) << " bytes" << endl;
+    cout << "The sizeof(long double) is : " << sizeof(long double) << " bytes" << endl;
+    cout << "The sizeof(bool) is : " << sizeof(bool) << " bytes" << endl;
+    return 0;
 
 
 
