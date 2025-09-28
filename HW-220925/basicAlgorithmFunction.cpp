bb667f55118bc451fd0bde4acaf78ca29142ca58
ntthanh0504 <ntt.thanh0504@gmail.com>
2025-09-28 13:52:37 +0700

    Fix syntax of function in basicAlgorithmFunction file

--- HW-220925/basicAlgorithmFunction.cpp
+++ HW-220925/basicAlgorithmFunction.cpp
@@ -1,168 +1,108 @@
 /* 10 bài */
 #include <iostream>
+#include <string>
 using namespace std;
-/* int resultEx1(int a, int b){
-    int resultE1;
-    if (a == b){
-        return resultE1 = (a + b) / 3;
-    } else {
-        return resultE1 = a + b;
-    }
-}
-int main() {
-    int a;
-    int b;
-    int resultE1;
-    cout << "type value of a: ";
-    cin >> a;
-    cout << "Type value of b: ";
-    cin >> b;
-    resultE1 = resultEx1(a, b);
-    cout << "Result: " << resultE1 << "\n";
-} */
 
-/* // Example 2:
-int resultEx2(int n) {
-    int resultE2;
+int example1(int a, int b){
+    int result;
+    if (a == b){
+        result = (a + b) / 3;
+    } else {
+        result = a + b;
+    }
+    return result;
+}
+
+// Example 2:
+int example2(int n) {
+    int result;
     int diff = n - 51;
+
     if (diff < 0){
         diff *= -1;
-        return resultE2;
     }
-    resultE2 = diff;
+
+    result = diff;
     if (n > 51)
     {
-        return resultE2 = diff * 3;
+        result = diff * 3;
     }
+    return result;
 }
-int main() {
-    cout << "type value of n: ";
-    int n;
-    cin >> n;
-    int resultE2;
-    resultE2 = resultEx2(n);
-    cout << "Result: " << resultE2 << "\n";
-}
- */
 
-/* // Example 3:
-bool resultEx3(int a, int b) {
-    bool resultE3;
+
+// Example 3:
+bool example3(int a, int b) {
+    bool result;
     if (a == 30 || b == 30 || a + b == 30)
     {
-    resultE3 = true;
-    return resultE3;
+        result = true;
     }
     else
     {
-    resultE3 = false;
-    return resultE3;
+        result = false;
     }
+    return result;
 }
-int main()
-{
-    int a, b;
-    cout << "Type value of a: ";
-    cin >> a;
-    cout << "Type value of b: ";
-    cin >> b;
-    bool resultE3;
-    resultE3 = resultEx3(a ,b);
-    cout << "Result: " << resultE3 << "\n";
-}
- */
+ 
 
-/* // Example 4:
-bool resultEx4(int a){
-   bool resultE4;
+// Example 4:
+bool example4(int a){
+   bool result;
    int lowerBoundOf100 = 90;
    int upperBoundOf100 = 110;
    int lowerBoundOf200 = 190;
    int upperBoundOf200 = 210;
    if ((a >= lowerBoundOf100 && a <= upperBoundOf100) || (a >= lowerBoundOf200 && a <= upperBoundOf200))
    {
-       resultE4 = true;
-       return resultE4;
+       result = true;
    }
    else
    {
-       resultE4 = false;
-       return resultE4;
+       result = false;
    }
+   return result;
 }
-int main(){
-   int a;
-   cout << "Type value of a: ";
-   cin >> a;
-   bool resultE4;
-   resultE4 = resultEx4(a);
-   cout << "Result: " << resultEx4 << "\n";
-} */
 
-/* // Example 5:
-string resultEx5(string s){
+// Example 5:
+string example5(string s){
     int length = s.length(); // do dai cua chuoi
-        string firstWord = "";
-        string resultEx5;
-        string resultE5 = "";
-        for (int i = 0; i <= length; i++){
-            if (s[i] == ' ')
-                break;
-            firstWord = firstWord + s[i];
-        }
-        if (firstWord == "if"){
-            resultE5 = s;
-            return resultE5;
-        } else{
-            resultE5 = "if " + s;
-            return resultE5;
-        }
+    string firstWord = "";
+    string result = "";
+    for (int i = 0; i <= length; i++){
+        if (s[i] == ' ')
+            break;
+        firstWord = firstWord + s[i];
+    }
+    if (firstWord == "if"){
+        result = s;
+    } else{
+        result = "if " + s;
+    }
+    return result;
 }
-int main() {
-    string s;
-    cout << "Type string value of s: ";
-    getline(cin, s);
-    string resultE5 = "";
-    resultE5 = resultEx5(s);
-    cout << "Result: "<< resultE5 << "\n";
 
-} */
-
-/* // Example 6:
-string resultEx6(string s)
+// Example 6:
+string example6(string s, int n)
 {
-    string resultE6 = "";
+    string result = "";
     int length = s.length();
-    int n;
     for (int i = 0; i < length; i++)
     {
         if (i == n)
-        {
             continue;
-            resultE6 += s[i];
-        }
-    }
-    return resultE6;
-}
-int main()
-{
-    string s;
-    cout << "Type value of s: ";
-    getline(cin, s);                                
-    int i;
-    cout << "type value of n: ";
-    cin >> i;
-    string resultE6 = "";
-    resultE6 = resultEx6(s);
-    cout << i << ":" << s[i] << ":" << resultE6 << "\n";
-}
-    Đang đứng hình
- */
 
- // Example 7:
- string resultEx7(string s){
+        result += s[i];
+    }
+    return result;
+}
+
+
+
+// Example 7:
+string example7(string s){
     int length = s.length();
-    string resultE7 = "";
+    string result = "";
     if (length != 1)
     {
         int firstPosition = 0;
@@ -170,100 +110,74 @@ int main()
         for (int i = 0; i < length; i++)
         {
             if (i == firstPosition)
-                resultE7 += s[lastPosition];
+                result += s[lastPosition];
             else if (i == lastPosition)
-                resultE7 += s[firstPosition];
+                result += s[firstPosition];
             else 
-                resultE7 += s[i];
+                result += s[i];
         }
-        return resultE7;
     } 
     else {
-        resultE7 = s;
+        result = s;
     }
-    return resultE7;
+    return result;
 
- }
- int main(){
-    string s;
-    cout << "Type value of s: ";
-    getline(cin, s);
-    string resultE7 = "";
-    resultE7 = resultEx7(s);
-    cout << "Result: " << resultE7 << "\n";
+}
+ 
+//Example 8:
+string example8(string s){
+    int length = s.length();
+    string result = "";
+    string twoFromChar = "";
+    if (length >= 2)
+    {
+        twoFromChar = s.substr(0, 2); // [0, 2) = [0, 1]
+        for (int i = 0; i < 4; i++){
+            result += twoFromChar;
+        } 
+    }
+    else
+    {
+        result = s;
+    }
+    return result;
 }
 
-/* //Example 8:
-string resultEx8(string s){
-     int length = s.length();
-       string resultE8 = "";
-       string twoFromChar = "";
-       if (length >= 2)
-       {
-           twoFromChar += s[0];
-           twoFromChar += s[1];
-           cout << twoFromChar << "\n";
-           for (int i = 0; i < 4; i++)
-           {
-               resultE8 += twoFromChar;
-           } 
-           return resultE8;
-       }
-       else
-       {
-           resultE8 = s;
-       }
-        return resultE8;
-}
-int main(){
-    string s;
-    cout << "Type value of s: ";
-    getline(cin, s);
-    string resultE8 = "";
-    resultE8 = resultEx8(s);
-    cout << "Result: " << resultE8 << "\n";
-} */
 
-/* // Example 9:
+// Example 9:
 string example9(string s){
     int length = s.length();
-    string resultEx9 = "";
+    string result = "";
     string lastCharacter = "";
     if ( length >= 1) {
         lastCharacter = s[length - 1];
-        resultEx9 = lastCharacter + s + lastCharacter;
+        result = lastCharacter + s + lastCharacter;
     }
-    return resultEx9;
+    return result;
 }
-int main(){
-    string s;
-    cout << "Type value of s: ";
-    getline(cin, s);
-    string resultEx9 = example9(s);
-    cout << "Result: "<< resultEx9 << "\n";
-} */
 
-/* // Example 10:
+// Example 10:
 bool example10 (int a) {
-    bool resultEx10;
+    bool result;
     if (a > 0)
     {
         if (a % 3 == 0 || a % 7 == 0)
         {
-            resultEx10 = true;
+            result = true;
         }
         else
         {
-            resultEx10 = false;
+            result = false;
         }
     }
-    return resultEx10;
+    return result;
 }
+
 int main(){
     int a;
-    bool resultEx10;
+    bool result;
     cout << "Type value of a: ";
     cin >> a;
-    resultEx10 = example10(a);
-    cout << "Result: " << resultEx10 << "\n";
-} */
+    result = example10(a);
+    cout << "Result: " << result << "\n";
+}
